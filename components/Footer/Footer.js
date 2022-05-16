ee9352d46dd60b2cfdc824525f9f1b4b2f5275cd
Cai Borui <756321586@qq.com>
2022-05-16 20:35:37 +0100

    add footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return(
+        <footer className="color-bg-secondary p-3" style={{minHeight: '50px'}} data-color-mode="dark" data-dark-theme="dark_dimmed">
+            <p className=" d-flex flex-justify-center">
+                Made with ❤️ by Cai Borui
+            </p>
+            <p className=" d-flex flex-justify-center">
+                © 2021 Cai Borui All rights reserved
+            </p>
+        </footer>
+    )
+}
--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,3 +1,4 @@
+import Footer from "../Footer/Footer"
 import Navbar from "../Navbar/NavBar"
 
 export default function Layout({ children }) {
@@ -5,6 +6,7 @@ export default function Layout({ children }) {
     <>
         <Navbar></Navbar>
         <main>{children}</main>
+        <Footer></Footer>
     </>
   )
 }
